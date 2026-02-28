2392f56f7c510d8f713d9da88df910d1f4ea6190
Jaid Lozano <jaidlozanoreyes@gmail.com>
2026-02-28 01:25:02 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,6 @@
 
-// Scroll botón
-function scrollToSection(id){
-document.getElementById(id).scrollIntoView({behavior:'smooth'});
-}
+document.getElementById("miFormulario").addEventListener("submit", function(e){
+    e.preventDefault();
 
-// Contadores animados
-const counters = document.querySelectorAll('.counter');
-counters.forEach(counter => {
-counter.innerText = '0';
-const updateCounter = () => {
-const target = +counter.getAttribute('data-target');
-const c = +counter.innerText;
-const increment = target / 200;
-if(c < target){
-counter.innerText = `${Math.ceil(c + increment)}`;
-setTimeout(updateCounter,10);
-}else{
-counter.innerText = target;
-}
-};
-updateCounter();
+    document.getElementById("mensaje").innerText = "Enviado correctamente ✅";
 });
-
-// Modal imágenes
-function openModal(src){
-document.getElementById('modal').style.display="flex";
-document.getElementById('modal-img').src=src;
-}
-
-function closeModal(){
-document.getElementById('modal').style.display="none";
-}
-
-// FAQ
-function toggleFAQ(btn){
-const p = btn.nextElementSibling;
-p.style.display = p.style.display === "block" ? "none" : "block";
-}
